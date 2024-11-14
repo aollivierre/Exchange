e5aed557558ce16a954f1c6f67ed22bf202778b0
aollivierre <abdullahollivierre@gmail.com>
2024-11-14 05:51:43 -0800

    creating remote mailboxes

--- Exchange/Send-SMTP-587.ps1
+++ Exchange/Send-SMTP-587.ps1
@@ -1,9 +1,9 @@
-$EmailFrom = "SMTP@contoso.com"
-$EmailTo = "aollivierre@contoso.com"
+$EmailFrom = "richard@richardd.ca"
+$EmailTo = "richard@richardd.ca"
 $Subject = "The subject of your email"
 $Body = "What do you want your email to say"
 $SMTPServer = "smtp.office365.com"
 $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
 $SMTPClient.EnableSsl = $true
-$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("SMTP@contoso.com", "Kav51862");
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("richard@richardd.ca", "yourpassword");
 $SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
